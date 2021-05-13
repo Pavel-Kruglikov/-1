6c4cc1057c4eb9ef86f6fa54180b841a3d43bedf
Pavel-Kruglikov <79210788+Pavel-Kruglikov@users.noreply.github.com>
2021-05-13 14:54:08 +0300

    LABA 4

---
+++ LABA 4/MyLib/DllCompile/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DllCompile
+{
+    public static class Class1
+    {
+        public static double Plus(double fir,double sec)
+        {
+            return fir + sec;
+        }
+        public static double Minus(double fir, double sec)
+        {
+            return fir - sec;
+        }
+        public static double Multi(double fir, double sec)
+        {
+            return fir * sec;
+        }
+        public static double Divide(double fir, double sec)
+        {
+            if (sec!=0)
+            {
+                return fir / sec;
+            }
+            else
+            {
+                Console.WriteLine("ошибка, знаменатель не может быть равен нулю\n код возвращаемый от операции с ошибкой равен 0 имеет и сообщение,например как это ");
+                return 0;
+            }
+            
+        }
+        public static double Ost(double fir,double sec)
+        {
+            
+            if (sec != 0)
+            {
+                return fir % sec;
+            }
+            else
+            {
+                Console.WriteLine("ошибка, знаменатель не может быть равен нулю\n код возвращаемый от операции с ошибкой равен 0 имеет и сообщение,например как это ");
+                return 0;
+            }
+        }
+    }
+}
---
+++ LABA 4/lab4_1/FourthLabNumberOne/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace FourthLabNumberOne
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Thread.Sleep(35);
+                for (int i = 0; i < 255; i++)
+                {
+                    if (GetAsyncKeyState(i)!=0)
+                    {
+                            Console.Write((char)i + " ");     
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+}
